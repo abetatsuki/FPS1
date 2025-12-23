24a066b9f786b5c4d1b441d6c32cfe9258c23522
kenkatou354 <kenkatou354@gmail.com>
2025-12-23 11:45:09 +0900

    [add]プレイヤーの視点クラスを追加

---
+++ Assets/FPS/Scripts/PlayerLook.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+namespace FPS.Scripts
+{
+    public class PlayerLook 
+    {
+        public  PlayerLook(Transform me, Transform camera)
+        {
+            _camera = camera;
+            _me = me;
+        }
+        public void ChangeLook(Vector2 LookInput)
+        {
+            (_yaw,_pitch) = CalculatorLook(LookInput, _speed, Time.deltaTime, _maxDt,_yaw,_pitch);
+            _me.rotation = Quaternion.Euler(0f, _yaw, 0f);
+            _camera.localRotation = Quaternion.Euler(_pitch, 0f, 0f);
+        }
+        public (float yaw, float pitch) CalculatorLook(Vector2 direction, float speed, float deltaTime,float maxDt,float yaw , float pitch)
+        {
+       
+            yaw += direction.x * speed * deltaTime;
+            pitch -= direction.y * speed * deltaTime;
+            pitch  = Mathf.Clamp(pitch, -8, maxDt);
+            return (yaw, pitch);
+
+        }
+        private float _speed = 5f;
+        private float _maxDt = 45f; 
+        private float _yaw = 0f;
+        private float _pitch = 0f;
+        private Transform _me;
+        private Transform _camera;
+    } 
+}
+
